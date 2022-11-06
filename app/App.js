105e74edee198d10e75713a0903074fccbb4de97
hengabay7 <101103416+hengabay7@users.noreply.github.com>
2022-11-06 13:21:28 +0200

    06112022

--- app/App.js
+++ app/App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  View, Text, StyleSheet,
+  TouchableOpacity
+} from 'react-native';
 
-export default function App() {
+
+const App = () => {
+
+
+  const doSomething = async () => {
+    const api = 'https://itunes.apple.com/search?term=metalica';
+    const respones = await fetch(api, {
+      method: 'get'
+    });
+    const data = await respones.json();
+    console.log(data);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={myStyle.container}>
+      <TouchableOpacity style={myStyle.btn} onPress={doSomething}>
+        <Text style={myStyle.btntext}>clike me</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
+
+
+const myStyle = StyleSheet.create({
+  btn: {
+    width: '100%', paddingVertical: 14,
+    alignContent: 'center', backgroundColor: '#17B890',
+    borderRadius: 14
+  },
+
+  btntext: { color: '#DEE5E5', fontSize: 22, fontWeight: '700' },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignitmes: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: '#DEE5E5'
+  }
+
 });
+
+export default App;
