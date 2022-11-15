562dde9004f3301c87d07a133fcb7ef71aea93bb
hengabay7 <101103416+hengabay7@users.noreply.github.com>
2022-11-15 09:13:38 +0200

    13112022

--- app/App.js
+++ app/App.js
@@ -1,31 +1,48 @@
 import React,{useState} from 'react';
 import {
   View, Text, StyleSheet,
-  TouchableOpacity,ActivityIndicator,
-} from 'react-native';
+  TouchableOpacity,ActivityIndicator,TextInput,FlatList} from 'react-native';
 
 
 const App = () => {
 
 
-const [searchName,setSearchName]=useState('Search');
+const [searchName,setSearchName]=useState('');
 const [isLoading,setIsloading]=useState(false);
+const [resulats,setResulats]=useState({});
 
 
   const doSomething = async () => {
-    const api = 'https://itunes.apple.com/search?term=Omer+adam';
+    setIsloading(true);
+    const api = `https://itunes.apple.com/search?term=${searchName}&entity=muisc`;
     const respones = await fetch(api, {
       method: 'get'
     });
     const data = await respones.json();
-    console.log(data);
+    setResulats(data.resulats);
+    setIsloading(false);
   }
+
+  //console.log(resulats);
 
   return (
     <View style={myStyle.container}>
       <View style={{height:'30%',flexDirection:'row'}}>
        <View style={{width:'70%'}}>
-
+       <TextInput 
+       style={{
+        width:'98%',
+        borderRadius:6,
+        paddingVertical:10,
+        backgroundColor:'#fff'
+       }}
+       value={searchName}
+       onChangeText={x=>{setSearchName(x)}}
+       autoCapitalize='none'
+       keyboardType="default"
+       placeholder="Type search"
+       secureTextEntry={false}
+       />
          </View>
          <View style={{width:'30%'}}>
          {
@@ -33,7 +50,7 @@ const [isLoading,setIsloading]=useState(false);
        ?( <ActivityIndicator size='large' color='#17B890' />)
        :(
           <TouchableOpacity style={myStyle.btn} onPress={doSomething}>
-            <Text style={myStyle.btntext}>{searchName}</Text>
+            <Text style={myStyle.btntext}>{"Search"}</Text>
           </TouchableOpacity>
         )
       }
@@ -43,8 +60,18 @@ const [isLoading,setIsloading]=useState(false);
       </View>
       <View style={{height:'70%'}}>
 
+      {     
+         <FlatList 
+         data={resulats}
+         keyExtractor={item=>item.trakId}
+         renderItem={itemRow=>
+         <View>
+         <Text>{itemRow.item}</Text>
+         </View>}
+         />     
+      }
+      
       </View>
-     
     </View>
   )
 }
@@ -53,22 +80,20 @@ const [isLoading,setIsloading]=useState(false);
 
 const myStyle = StyleSheet.create({
   btn: {
-    width:'100%', paddingVertical: 18,
+    width:'98%', paddingVertical: 18,
     alignItems: 'center', backgroundColor:'#17B890',
-    borderRadius: 14,
+    borderRadius: 6,
   },
 
   btntext: { 
-    color: '#DEE5E5',
-     fontSize: 24, 
+     color: '#DEE5E5',
+     fontSize: 22, 
      fontWeight: '700'
     },
 
   container: {
     flex: 1,
     padding:30,
-   alignItems: 'center',
-    justifyContent: 'center',
     backgroundColor: '#DEE5E5'
   }
 
